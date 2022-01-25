c6e5f511ce7aaebef5506b2b1335c8e09fb5c043
Robin <robin.duennwald@gmail.com>
2022-01-25 17:31:39 +0100

    add react icons

--- components/textareaInput.tsx
+++ components/textareaInput.tsx
@@ -1,13 +1,14 @@
 import { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import autosize from "autosize";
 import { ToDoItem } from "../interfaces/todos";
 import { activeToDo, editToDoItem } from "../services/gunDB";
+import { IoSend } from "react-icons/io5";
+import { useTranslation } from "next-i18next";
 
-interface InputTextareaProps {}
-
-const InputTextarea: NextPage = (props: InputTextareaProps) => {
+const InputTextarea: FunctionComponent = (props) => {
   let textarea: any;
+  const { t } = useTranslation();
 
   const [state, setState] = useState({ text: "" });
 
@@ -50,22 +51,24 @@ const InputTextarea: NextPage = (props: InputTextareaProps) => {
 
   return (
     <>
-      <form onSubmit={addItem} className="flex">
-        <textarea
-          ref={(c) => (textarea = c)}
-          placeholder="type some text"
-          rows={1}
-          onChange={textChange}
-          onKeyPress={handleKeyPress}
-          className="dark:bg-gray-600 w-80 text-4xl dark:text-lime-300 text-lime-600 rounded-xl p-0.5"
-        />
-        <button
-          type="submit"
-          className="dark:hover:bg-lime-600 hover:bg-lime-700 font-bold rounded dark:bg-lime-300 bg-lime-600 text-white dark:text-slate-900 text-center cursor-pointer p-2"
-        >
-          <img src="/icons/send-svgrepo-com.svg" />
-        </button>
-      </form>
+      <section className="m-auto">
+        <form onSubmit={addItem} className="flex justify-center items-center">
+          <textarea
+            ref={(c) => (textarea = c)}
+            placeholder={t("common:your")}
+            rows={1}
+            onChange={textChange}
+            onKeyPress={handleKeyPress}
+            className="dark:bg-gray-600 w-96 text-4xl dark:text-lime-300 text-lime-600 rounded-xl p-0.5"
+          />
+          <button
+            type="submit"
+            className="mx-2 w-10 h-10 dark:hover:bg-lime-600 hover:bg-lime-700 font-bold rounded-xl dark:bg-lime-300 bg-lime-600 text-white dark:text-slate-900 text-center cursor-pointer p-2"
+          >
+            <IoSend className="w-full h-full" />
+          </button>
+        </form>
+      </section>
     </>
   );
 };
