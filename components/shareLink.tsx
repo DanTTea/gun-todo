aae66b483f69308c06a42b8071777c15d6c9e80b
Robin <robin.duennwald@gmail.com>
2022-01-26 13:46:40 +0100

    add clipboard copy function

--- components/shareLink.tsx
+++ components/shareLink.tsx
@@ -3,21 +3,29 @@ import { FunctionComponent, useEffect, useState } from "react";
 interface ShareLinkProps {}
 
 const ShareLink: FunctionComponent<ShareLinkProps> = () => {
-  const [state, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     setIsVisible(true);
   }, []);
 
+  const copyToClipboard = (event: { target: any }) => {
+    const { target } = event;
+
+    return navigator.clipboard.writeText(target.innerHTML);
+  };
+
   return (
     <>
-      {state ? (
+      {isVisible ? (
         <div className="text-center text-sm m-8 transition duration-200">
           <div className="text-gray-400">
             Teile folgenden Link um zusammen an der Todo-liste zu arbeiten
           </div>
 
-          <p className="dark:text-lime-300">{document.location.href}</p>
+          <p onClick={copyToClipboard} className="dark:text-lime-300">
+            {document.location.href}
+          </p>
         </div>
       ) : (
         <div></div>
