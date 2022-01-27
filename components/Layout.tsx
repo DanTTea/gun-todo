d3fd75dbf65d91b434bf08c136e91beb2e9daab9
Robin <robin.duennwald@gmail.com>
2022-01-27 14:00:04 +0100

    add meta tags

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -19,6 +19,15 @@ export default function Layout({ children }: LayoutProps) {
     <Theme dark={state.dark}>
       <Head>
         <title>Gun Todo Dapp</title>
+        <meta
+          property="og:image"
+          content="https://camo.githubusercontent.com/64213f411349db936a0fa36ef41741b170d4c8d34d1cc0d1c887f7d880838707/68747470733a2f2f636c6475702e636f6d2f5445793979476834356c2e737667"
+        />
+        <meta property="og:type" content="dapp" />
+        <meta
+          property="og:description"
+          content="Decentralised Todo App! Share and securely collaborate on your todo list!"
+        />
       </Head>
       <Header themeChange={onThemeChange} />
       <main className="bg-gray-100 dark:bg-gray-900 text-black  dark:text-white min-h-full">
