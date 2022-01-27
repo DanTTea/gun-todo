44170595eab4275dcb1e673ec0a8b6ea45ac9452
Robin <robin.duennwald@gmail.com>
2022-01-27 13:37:54 +0100

    add html header and meta tag

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useState } from "react";
 import Header from "./Header";
 import Theme from "./Theme";
@@ -16,6 +17,13 @@ export default function Layout({ children }: LayoutProps) {
 
   return (
     <Theme dark={state.dark}>
+      <Head>
+        <title>Gun Todo Dapp</title>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        ></meta>
+      </Head>
       <Header themeChange={onThemeChange} />
       <main className="bg-gray-100 dark:bg-gray-900 text-black  dark:text-white min-h-full">
         {children}
